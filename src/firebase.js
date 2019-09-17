c05776936416fd987d18335cd9f432e892c87f0e
ItzelMB <it.mobec@gmail.com>
2019-09-17 16:50:32 -0500

    login con Google

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAVZOOtH8yXyFptjC50vwl017rLcKUWxrw",
+    authDomain: "labnotes-98af6.firebaseapp.com",
+    databaseURL: "https://labnotes-98af6.firebaseio.com",
+    projectId: "labnotes-98af6",
+    storageBucket: "",
+    messagingSenderId: "1027985148328",
+    appId: "1:1027985148328:web:f00fafb57a001318ee759d"
+}
